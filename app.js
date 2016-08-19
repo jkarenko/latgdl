5d35bd1e1ae9ce9d7a3c1074c89d94b896286b57
jkarenko <jk@noo.se>
2016-08-19 21:14:17 +0300

    add post commenting in frontend
    this was the last of the frontend skeleton for now - moving on to the node backend.

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.config([
   }
 ]);
 
+
+// factories
+
 app.factory('posts', [function(){
   var o = {
     posts: []
@@ -34,31 +37,65 @@ app.factory('posts', [function(){
   return o;
 }]);
 
+// controllers
+
 app.controller('MainCtrl', [
   '$scope',
   'posts',
   function($scope, posts) {
-    $scope.test = 'Hello World!';
-
     $scope.posts = posts.posts;
 
     for (var i = 0; i < 5; i++) {
-      $scope.posts.push({title: 'post ' + i, upvotes: i});
+      $scope.posts.push({
+        title: 'post ' + i,
+        upvotes: i,
+        comments: [
+          {author: 'Bob', body: 'Cool post, bro!', upvotes: 2},
+          {author: 'Mr. Stevens', body: 'GOAT', upvotes: 3}
+        ]
+      });
     }
 
     $scope.addPost = function() {
       if (!$scope.title || $scope.title === '') { return; }
+
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Bob', body: 'Cool post, bro!', upvotes: 2},
+          {author: 'Mr. Stevens', body: 'GOAT', upvotes: 3}
+        ]
       });
+
       $scope.title = '';
       $scope.link = '';
-    }
+    };
 
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     };
   }
+]);
+
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts) {
+
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if ($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
+  }
+
 ]);
