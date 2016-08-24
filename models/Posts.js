092e1c21868aa37e1925a1cd28b26f3d73ff8d49
jkarenko <jk@noo.se>
2016-08-24 21:35:40 +0300

    fix idiot typos
    for best results always code when extremely tired

--- models/Posts.js
+++ models/Posts.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 var PostSchema = new mongoose.schema({
   title: String,
   link: String,
-  upvotes = { type: Number, default 0 },
-  comments [{type: mongoose.Schema.Types.ObjetctId, ref: 'Post'}]
+  upvotes: { type: Number, default 0 },
+  comments: [{type: mongoose.Schema.Types.ObjetctId, ref: 'Post'}]
 });
